a463076acb8066d17a9d08f561dde4edbabbe538
samankey <71953671+samankey@users.noreply.github.com>
2022-12-26 22:12:50 +0900

    Feat: 프로토타입 완성

--- pages/index.vue
+++ pages/index.vue
@@ -1,62 +1,73 @@
 <template>
-  <div class="wrap">
-    <div @click="getPhoto">안녕</div>
-    <div class="par">
-      <img :src="imageSrc" alt="imageSrc" />
-      <div class="filter" />
+  <div class="box">
+    <div class="wrap">
+      <div class="canvas" :style="`background-color: ${color}`">
+        <p class="text fill Headline">안녕안녕</p>
+        <p class="text line Headline">안녕안녕</p>
+        <p class="text fill Headline">🦄</p>
+        <p class="text line Headline">apple</p>
+        <p class="text fill Headline">🍟</p>
+        <p class="text fill Headline">⚽️</p>
+        <p class="text fill Headline">sorry</p>
+        <p class="text line Headline">my name is</p>
+        <p class="text fill Headline">🦄</p>
+        <p class="text line Headline">안녕안녕</p>
+        <p class="text fill Headline">🍟</p>
+        <p class="text fill Headline">⚽️</p>
+      </div>
+      <input v-model="color" type="color" />
     </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
 export default {
   name: 'IndexPage',
 
   data() {
     return {
-      imageSrc: '',
+      color: '',
     }
   },
 
   methods: {
-    getPhoto() {
-      try {
-        axios
-          .get('https://api.unsplash.com/photos/random', {
-            params: {
-              client_id: 'bVPTgly_pHzoZu1lRrCUMw9imwIo0RTspGo3Vg_EwV0',
-              topics: 't/3d-renders',
-            },
-          })
-          .then((res) => {
-            this.imageSrc = res.data.urls.regular
-          })
-      } catch (err) {
-        alert(err)
-      }
-    },
+    getPhoto() {},
   },
 }
 </script>
 
 <style lang="scss" scoped>
-.wrap {
+.box {
   @include flex-item(center);
-  .par {
-    position: relative;
-    img {
+  padding: 1rem;
+  .wrap {
+    .canvas {
+      display: flex;
+      align-content: flex-start;
+      align-items: flex-start;
+      flex-wrap: wrap;
+      padding: 2rem;
       width: 640px;
       height: 360px;
-      object-fit: cover;
-    }
-    .filter {
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: 640px;
-      height: 360px;
-      backdrop-filter: blur(10px);
+      filter: drop-shadow(0 9px 46px rgba(0, 0, 0, 0.01))
+        drop-shadow(0 11px 15px rgba(0, 0, 0, 0.04))
+        drop-shadow(0 24px 38px rgba(0, 0, 0, 0.01))
+        drop-shadow(0 -3px 5px rgba(95, 70, 70, 0.05));
+
+      .text {
+        margin: 0.25rem;
+        padding: 0.5rem 0.75rem;
+        border-radius: 6.25rem;
+        word-break: break-all;
+      }
+      .fill {
+        background-color: aquamarine;
+        border: 3px solid aquamarine;
+      }
+
+      .line {
+        border: 3px solid aquamarine;
+      }
     }
   }
 }
