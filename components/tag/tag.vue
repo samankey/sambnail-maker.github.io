949f936473b466d093f28cabf20c8449474e7f21
samankey <71953671+samankey@users.noreply.github.com>
2022-12-28 23:34:19 +0900

    Feat: theme 설정

---
+++ components/tag/tag.vue
@@ -0,0 +1,40 @@
+<template>
+  <span class="text Headline" :class="getTheme">
+    {{ tag.text }}
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    tag: {
+      type: Object,
+      default: () => {}
+    },
+
+    className: {
+      type: Object,
+      default: () => {}
+    }
+  },
+
+  computed: {
+    getTheme() {
+      return this.tag.type === 'fill'
+        ? this.className.fill
+        : this.tag.type === 'border'
+        ? this.className.border
+        : this.className.ghost;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.text {
+  margin: 0.25rem;
+  padding: 0.5rem 0.75rem;
+  border-radius: 6.25rem;
+  word-break: break-all;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,32 @@
 <template>
   <div class="box">
     <div class="canvas bd_Gray">
-      <div class="textBox">
-        <span
+      <div v-if="tagList.length > 0" class="textBox">
+        <Tag
           v-for="(tag, index) in tagList"
           :key="index"
-          class="text fill Headline"
-        >
-          {{ tag }}
-        </span>
+          :tag="tag"
+          :class-name="getTheme"
+        />
       </div>
+    </div>
+    <div class="typeButton">
+      <button
+        v-for="(type, index) in types"
+        :key="index + type"
+        @click="setType(type)"
+      >
+        {{ type }}
+      </button>
+    </div>
+    <div class="typeButton">
+      <button
+        v-for="(color, index) in themeList"
+        :key="index + color"
+        @click="setTheme(color.title)"
+      >
+        {{ color.title }}
+      </button>
     </div>
     <input v-model="tagText" type="text" />
     <button @click="addTag">추가</button>
@@ -17,64 +34,79 @@
 </template>
 
 <script>
+import themeList from '@/assets/data/theme.json';
+import Tag from '@/components/tag/tag';
+
 export default {
-  name: 'IndexPage',
+  components: {
+    Tag
+  },
 
   data() {
     return {
-      // totalByte: 0,
       height: 0,
       tagText: '',
+      tagType: 'fill',
       tagList: [],
-    }
+      types: ['fill', 'border', 'ghost'],
+      themeTitle: 'w_b',
+      themeList: themeList.color
+    };
   },
 
   computed: {
-    //   calcTextLengthByByte() {
-    //     const emojiExp = /(\p{Emoji_Presentation}|\p{Extended_Pictographic})/gu
-    //     const textList = this.tagList.join('').replace(emojiExp, 'A').split('')
-    //     const byte = textList
-    //       .map((text) => {
-    //         const code = text.charCodeAt(0)
-    //         return code > 127 || (code > 64 && code < 91) ? 2 : 1
-    //       })
-    //       .reduce((acc, cur) => {
-    //         return acc + cur
-    //       }, 0)
-    //     return byte
-    //   },
+    getTheme() {
+      const filteredTheme = this.themeList.filter(
+        (item) => item.title === this.themeTitle
+      );
+      const theme = filteredTheme[0];
+      return {
+        fill: `${theme.bg} ${theme.fill} ${theme.bd} ${theme.fill_ft}`,
+        border: `${theme.bd} ${theme.bd_ft}`,
+        ghost: `${theme.bd_ft} bd_transparent`
+      };
+    }
   },
 
   updated() {
-    this.setHeight()
+    this.setHeight();
   },
 
   methods: {
+    setType(type) {
+      this.tagType = type;
+    },
+
+    setTheme(themeTitle) {
+      this.themeTitle = themeTitle;
+    },
+
     addTag() {
+      const text = { text: this.tagText, type: this.tagType };
       if (!this.tagText) {
-        alert('없어')
+        alert('없어');
       } else {
-        this.tagList.push(this.tagText)
-        this.tagText = ''
+        this.tagList.push(text);
+        this.tagText = '';
+        this.tagType = 'fill';
       }
     },
 
     setHeight() {
-      const height = document.querySelector('.textBox')?.offsetHeight
-      this.height = height
+      const height = document.querySelector('.textBox')?.offsetHeight;
+      this.height = height;
       if (this.height > 256) {
-        this.tagList.pop()
-        alert('끝')
+        this.tagList.pop();
+        alert('끝');
       }
-      console.log(this.height)
-    },
-  },
-}
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
 .box {
-  @include flex-item(center);
+  text-align: center;
   padding: 1rem;
 
   .canvas {
@@ -87,21 +119,6 @@ export default {
       align-content: flex-start;
       align-items: flex-start;
       flex-wrap: wrap;
-
-      .text {
-        margin: 0.25rem;
-        padding: 0.5rem 0.75rem;
-        border-radius: 6.25rem;
-        word-break: break-all;
-      }
-      .fill {
-        background-color: aquamarine;
-        border: 3px solid aquamarine;
-      }
-
-      .line {
-        border: 3px solid aquamarine;
-      }
     }
   }
 }
